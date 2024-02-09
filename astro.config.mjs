3496cac5526c7cdda1d587833255617cf3cea600
joczikszabi <joczikszabi@gmail.com>
2024-02-09 05:24:02 +0100

    improved lighthouse rating

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), icon({
+    iconDir: "src/assets/icons",
+    include: {
+      mdi: ["checkbox-marked-circle-outline"], // Loads only Material Design Icon's "account" SVG
+    },
+  }),]
 });
