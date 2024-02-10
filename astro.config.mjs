dea4d8bfaa9b4e2c0c90f6386325426d92aec163
joczikszabi <joczikszabi@gmail.com>
2024-02-10 14:53:36 +0100

    added analytics, speedinsight

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,23 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), icon({
     iconDir: "src/assets/icons",
     include: {
-      mdi: ["checkbox-marked-circle-outline"], // Loads only Material Design Icon's "account" SVG
+      mdi: ["checkbox-marked-circle-outline"] // Loads only Material Design Icon's "account" SVG
+    }
+  })],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
     },
-  })]
+    speedInsights: {
+      enabled: true,
+    },
+  }),
 });
