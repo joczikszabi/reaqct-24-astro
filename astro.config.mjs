c97c4aa4a8ee028a033ce75810ac942bf949f50d
joczikszabi <joczikszabi@gmail.com>
2024-02-10 14:26:21 +0100

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from '@astrojs/vercel/static';
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -10,5 +11,7 @@ export default defineConfig({
     include: {
       mdi: ["checkbox-marked-circle-outline"], // Loads only Material Design Icon's "account" SVG
     },
-  }),]
+  })],
+  output: 'static',
+  adapter: vercelStatic(),
 });
