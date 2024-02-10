150439f3b34e2f18f8b562d48c077f3b090e3a15
joczikszabi <joczikszabi@gmail.com>
2024-02-10 14:35:03 +0100

    renamed incorrectly named paths + remove vercel plugin for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from '@astrojs/vercel/static';
 import icon from "astro-icon";
 
 // https://astro.build/config
@@ -11,7 +10,5 @@ export default defineConfig({
     include: {
       mdi: ["checkbox-marked-circle-outline"], // Loads only Material Design Icon's "account" SVG
     },
-  })],
-  output: 'static',
-  adapter: vercelStatic(),
+  })]
 });
