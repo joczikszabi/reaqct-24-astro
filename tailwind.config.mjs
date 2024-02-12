721b3752546a82485e720b1dd1803debab4c81d4
joczikszabi <joczikszabi@gmail.com>
2024-02-12 13:30:49 +0100

    trying again with font family support

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
 	theme: {
 		extend: {
 		  fontFamily: {
-			  sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+			  sans: ["Montserrat Variable", ...defaultTheme.fontFamily.sans],
 		  },
 		  colors: {
 			primary: {
