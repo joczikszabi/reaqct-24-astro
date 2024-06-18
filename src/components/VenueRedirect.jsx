8ef18cd4d5e5eb3e5134b0a6a7f2e452f1aad7c1
joczikszabi <joczikszabi@gmail.com>
2024-06-18 09:55:22 +0200

    added venue + removed nav items + changed sponsor page

---
+++ src/components/VenueRedirect.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function VenueRedirect() {
+  window.location.href = 'https://reaqct24storage.blob.core.windows.net/assets/ReAQCT_Venue.pdf?sp=r&st=2024-06-18T07:54:07Z&se=2025-03-21T16:54:07Z&spr=https&sv=2022-11-02&sr=b&sig=7r5AH7Al2HdKVmIjnbdINdqopLI35%2BNEXOQJU7o%2Fvk4%3D';
+}
