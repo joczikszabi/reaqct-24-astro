3c9746203af504fa1d044ec84a692599ea3da6e5
joczikszabi <joczikszabi@gmail.com>
2024-07-24 15:37:40 +0200

    fixed issue with paths of static images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/static";
 import icon from "astro-icon";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +14,5 @@ export default defineConfig({
     webAnalytics: {
       enabled: true
     }
-  })
+  }),
 });
