018af00f39732389d0c5f1c71d089cdbb92138a0
joczikszabi <joczikszabi@gmail.com>
2024-10-04 10:49:49 +0200

    removed vercel, added docker and github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 import icon from "astro-icon";
-
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), icon()],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  }),
 });
